cc2c30f24af0549fbde07f143390f30a5f3e8780
Jean-Marc Meessen <jean-marc@meessen-web.org>
2018-07-29 18:10:38 +0200

    Add ssh connection capability

--- notes.md
+++ notes.md
@@ -10,3 +10,7 @@
 - Sanity check: verify with `terraform plan`
 - Start the environment: `terraform apply`
 - Pull the environement down: `terraform destroy`
+
+- generate an SSH key with `ssh-keygen -t rsa -b 2048 -v` in the `~/.ssh` directory.
+- copy the public key in the terraform directory so that it can be found in the scripts
+- connect to the server with `ssh <ip-address> -l ubuntu -i ~/.ssh/captains_aws_cjp`
---
+++ terraform/security-group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "jmm_sg" {
+  name = "JMM_sg"
+
+  ## Allow Internet Access for everyone
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
