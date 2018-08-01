117399f3dc0067ea82b032de4d5f9d3cb61901c8
Jean-Marc Meessen <jean-marc@meessen-web.org>
2018-08-01 16:13:16 +0200

    Add incoming connections on 8080

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -22,4 +22,11 @@ resource "aws_security_group" "jmm_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
