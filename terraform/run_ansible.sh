33676846ac7aa4601981c7449fb8aedcdd2c4ab0
Jean-Marc Meessen <jean-marc@meessen-web.org>
2018-08-03 15:17:54 +0200

    Moved the ansible scripts one level up

--- terraform/run_ansible.sh
+++ terraform/run_ansible.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory  -u ubuntu --key-file "~/.ssh/captains_aws_cjp" deploy/playbook.yml
+ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory  -u ubuntu --key-file "~/.ssh/captains_aws_cjp" ../deploy/playbook.yml
