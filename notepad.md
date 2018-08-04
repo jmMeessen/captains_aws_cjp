4c8c0b482ee0a8215b1157f0cf91bc00925da15f
Jean-Marc Meessen <jean-marc@meessen-web.org>
2018-08-04 17:20:18 +0200

    enable interconnection to connect a Client Master

--- notepad.md
+++ notepad.md
@@ -1,1 +1,4 @@
 # Random notes while Work is in progress
+
+- should limit the access to the port 5000 to only the machines in the cluster
+- during the CM registration process, there are connection that need to happen to the standart URL. For ease of use, will change the port to use the default port 80.
--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -26,6 +26,28 @@ resource "aws_security_group" "jmm_general_sg" {
 resource "aws_security_group" "jmm_cjoc_sg" {
   name = "JMM_cjoc_sg"
 
+  egress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # So that the CJOC can connect to the CLient Master URL
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 8888
     to_port     = 8888
@@ -36,6 +58,28 @@ resource "aws_security_group" "jmm_cjoc_sg" {
 
 resource "aws_security_group" "jmm_master_sg" {
   name = "JMM_master_sg"
+
+  egress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # So that the Client Master can connect to the CJOC URL
+  egress {
+    from_port   = 8888
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 8080
