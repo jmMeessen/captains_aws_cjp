9ccf9fa546b3a1180a798624cf984b11b4a34ebc
Jean-Marc Meessen <jean-marc@meessen-web.org>
2019-02-06 14:05:25 +0100

    WIP

---
+++ deploy/roles/jenkins/files/configure-client-master.groovy
@@ -0,0 +1,45 @@
+// Imports
+import com.cloudbees.opscenter.server.model.ClientMaster
+import com.cloudbees.opscenter.server.model.ConnectedMaster
+import com.cloudbees.opscenter.server.model.OperationsCenter
+import com.cloudbees.opscenter.server.properties.ConnectedMasterLicenseServerProperty
+import jenkins.model.Jenkins
+
+// Input parameters
+def clientMasterName = "CM-1"
+// def clientMasterId = 5
+// def clientMasterGrantId = "fjs2ktwfgd"
+def clientMasterLicenseStrategy = new ConnectedMasterLicenseServerProperty.FloatingExecutorsStrategy() // or  ConnectedMasterLicenseServerProperty.NoLicensingStrategy();
+
+// Compute some values based on the input parameters
+int clientMasterId = Jenkins.instance.getAllItems(ClientMaster.class).size() + 1
+String clientMasterGrantId = clientMasterId + '-' + clientMasterName 
+
+
+Jenkins jenkins = Jenkins.instance
+
+if (jenkins.getItem(clientMasterName)==null){
+	// Create Client Master Declaration
+	ClientMaster cm = OperationsCenter.instance.createClientMaster(clientMasterName)
+
+	// Another way to create a ClientMaster is as follow
+	//ClientMaster cm = Jenkins.instance.createProject(ClientMaster.class, clientMasterName)
+
+	//Set Client Master properties
+	cm.setId(clientMasterId)
+	cm.setIdName(ConnectedMaster.createIdName(clientMasterId, clientMasterName))
+	cm.setGrantId(clientMasterGrantId)
+	cm.getProperties().replace(new ConnectedMasterLicenseServerProperty(clientMasterLicenseStrategy))
+	cm.save()
+
+	if (OperationsCenter.instance.getConnectedMasterByName(cm.idName)!=null){
+    	println "Created ClientMaster '${cm.name}' known as '${cm.idName}'"
+    	println "-DMASTER_INDEX=${cm.id}'"
+    	println "-DMASTER_NAME=${cm.name}'"
+    	println "-DMASTER_GRANT_ID=${cm.grantId}'"
+	} else {
+    	println "[ERROR:]" + clientMasterName + "has not been created in CJOC"
+	}
+} else {
+  println "[WARNING] " + clientMasterName + " already exists in CJOC"
+}
