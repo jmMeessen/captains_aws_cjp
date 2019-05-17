768d7fa45bd26e4e0dd1c565b9df64d81f44787a
Jean-Marc Meessen <jean-marc@meessen-web.org>
2019-05-17 10:45:13 +0200

    Finally a good display for 'is_infra_running.sh'

--- toolbox/is_infra-running.sh
+++ toolbox/is_infra-running.sh
@@ -3,8 +3,4 @@
 
 ./update_aws_token.sh
 
-#aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters Name=tag:Owner,Values=Jmm --output text --query "Reservations[].Instances[].[InstanceId]"
-#aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters Name=tag:Owner,Values=Jmm --output text --query "Reservations[].Instances[].Tags[?Key==\`Name\`].Value[]"
-#aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters "Name=tag:Owner,Values=Jmm" --output text --query "Reservations[].Instances[].State.[Name]"
-#aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters "Name=tag:Owner,Values=Jmm" --output text --query "Reservations[*].Instances[*].[InstanceId, State.Name]"
-aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters "Name=tag:Owner,Values=Jmm" --output text --query "Reservations[*].Instances[*].[InstanceID, State.Name, Tags[?Key==\`Name\`].Value[]]"
+aws --region us-east-1 --profile cloudbees-ps  ec2 describe-instances --filters "Name=tag:Owner,Values=Jmm" --output table --query "Reservations[*].Instances[*].{Name:Tags[?Key==\`Name\`] | [0].Value,State:State.Name}"
