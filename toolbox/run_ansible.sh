dc322aed8966b2dea781781f5ca94f5e9f2f3895
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2019-05-19 13:00:06 +0200

    start to configure the docker agent

--- toolbox/run_ansible.sh
+++ toolbox/run_ansible.sh
@@ -2,4 +2,4 @@
 
 export TF_STATE=../terraform/terraform.tfstate
 
-ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory  -u ubuntu --key-file "~/.ssh/captains_aws_cjp" --vault-password-file .work-password ../deploy/playbook.yml
+ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory -u ubuntu --key-file "~/.ssh/captains_aws_cjp" --vault-password-file .work-password ../deploy/playbook.yml
