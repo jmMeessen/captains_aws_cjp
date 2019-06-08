56b0348341fb17102c668ada610482d8656ddeb7
Jean-Marc Meessen <jean-marc@meessen-web.org>
2019-06-08 14:08:15 +0200

    Fixed the name resolution issue

--- toolbox/run_ansible.sh
+++ toolbox/run_ansible.sh
@@ -2,4 +2,6 @@
 
 export TF_STATE=../terraform/terraform.tfstate
 
+. ./gather_IPs.sh 
+
 ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory -u ubuntu --key-file "~/.ssh/captains_aws_cjp" --vault-password-file .work-password ../deploy/playbook.yml
