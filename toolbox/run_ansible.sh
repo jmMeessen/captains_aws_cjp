9a790f7bb7cdecc9f7ba23968f7be14cdbb670e9
Jean-Marc MEESSEN <jean-marc@meessen-web.org>
2019-09-19 11:09:12 +0200

    Add timing to ansible script

--- toolbox/run_ansible.sh
+++ toolbox/run_ansible.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+start=`date +%s`
+
 export TF_STATE=../terraform/terraform.tfstate
 
 . ./gather_IPs.sh 
 
 ansible-playbook --inventory-file=/usr/local/bin/terraform-inventory -u ubuntu --key-file "~/.ssh/captains_aws_cjp" --vault-password-file .work-password ../deploy/playbook.yml
+
+end=`date +%s`
+echo Execution time was `expr $end - $start` seconds.
