10b79763b554584115631bb93d50442a35f5a44c
Jean-Marc Meessen <jean-marc@meessen-web.org>
2021-02-20 20:00:08 +0100

    Generate a token automatically

---
+++ deploy/roles/jenkins/files/generateToken.groovy
@@ -0,0 +1,30 @@
+import hudson.model.User
+import jenkins.security.ApiTokenProperty
+import java.util.logging.Logger
+
+String scriptName = "create-k8s-secret-with-jenkins-token.groovy"
+Logger logger = Logger.getLogger(scriptName)
+logger.info("Starting ${scriptName}")
+
+def userName = 'admin'
+def jenkinsTokenName = 'defaultAdminToken'
+def tokenFileName = 'secrets/defaultAdminToken'
+def env = System.getenv()
+def jenkins_home = env['JENKINS_HOME']
+
+def user = User.get(userName, false)
+def apiTokenProperty = user.getProperty(ApiTokenProperty.class)
+def tokens = apiTokenProperty.tokenStore.getTokenListSortedByName().findAll {it.name==jenkinsTokenName}
+
+if(tokens.size() != 0) {
+    logger.info("Token exists. Revoking any with this name and recreating to ensure we have a valid value stored in the secret.")
+    tokens.each {
+        apiTokenProperty.tokenStore.revokeToken(it.getUuid())
+    }
+}
+
+def result = apiTokenProperty.tokenStore.generateNewToken(jenkinsTokenName).plainValue
+user.save()
+
+File file = new File(jenkins_home + "/" + tokenFileName)
+file.write result + "\n"
